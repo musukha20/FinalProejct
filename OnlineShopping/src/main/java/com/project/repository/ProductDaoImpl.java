442a23b96bf8245f44239ead5e0feb784de816ee
AbheekRawal <abkrawal29@gmail.com>
2020-10-03 11:46:55 +0530

    added search function in product dao

--- OnlineShopping/src/main/java/com/project/repository/ProductDaoImpl.java
+++ OnlineShopping/src/main/java/com/project/repository/ProductDaoImpl.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import com.project.dto.ProductDto;
 import com.project.entity.Product;
 
 public class ProductDaoImpl implements ProductDao {
@@ -15,13 +16,27 @@ public class ProductDaoImpl implements ProductDao {
 	protected EntityManager entityManager;
 	
 	@Override
-	public List<Product> search(String keyword) {
-		List<Product> products = new ArrayList<Product>();
+	public List<ProductDto> search(String keyword) {
+		List<ProductDto> products = new ArrayList<ProductDto>();
 		String query = "select p.* from Product p where brand =:x or category:=y or name like :=z";
 		Query q = (Query) this.entityManager.createQuery(query);
 		q.setParameter("x", keyword);
 		q.setParameter("y", keyword);
 		q.setParameter("z", "%"+keyword+"%");
+		List<Product> product = q.getResultList();
+		for(Product p: product) {
+			int productId = p.getProductId();
+			String productImage1 = p.getProductImage1();
+			String productName = p.getName();
+			String productBrand = p.getBrand();
+			Double productPrice = p.getPrice();
+			String productDescription = p.getDescription();
+			String productImage2 = p.getProductImage2();
+			String productImage3 = p.getProductImage3();
+			String productImage4 = p.getProductImage4();
+			products.add(new ProductDto(productImage1, productImage2, productImage3,productImage4,productDescription,
+					productId, productName, productBrand,productPrice));
+		}
 		return products;
 	}
 
