349118d31a03a40524a6c7ef423ddbf93bc05bf8
AbheekRawal <abkrawal29@gmail.com>
2020-10-04 02:15:53 +0530

    New changes

--- OnlineShopping/src/main/java/com/project/entity/Cart.java
+++ OnlineShopping/src/main/java/com/project/entity/Cart.java
@@ -25,10 +25,32 @@ public class Cart {
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private Product product;
+	
+	//private int product_id;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
+	
+	//private int user_id;
+
+	
+
+	/*public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	} */
 
 	public int getId() {
 		return id;
--- OnlineShopping/src/main/java/com/project/repository/CartDaoImpl.java
+++ OnlineShopping/src/main/java/com/project/repository/CartDaoImpl.java
@@ -18,8 +18,12 @@ public class CartDaoImpl implements CartDao {
 
 	@Override
 	public boolean addToCart(int userId, int productId) {
+		System.out.println(userId);
+		System.out.println(productId);
 		User user = this.entityManager.find(User.class, userId);
 		Product product = this.entityManager.find(Product.class, productId);
+		System.out.println(user);
+		System.out.println(product);
 		Cart cart = new Cart();
 		
 		cart.setQuantity(1); 
