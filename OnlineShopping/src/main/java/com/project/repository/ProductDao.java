36e2cc4e9d9b83b405a7974c11cbb4ff9d19e9bc
AbheekRawal <abkrawal29@gmail.com>
2020-10-05 14:44:13 +0530

    Added List of Products

--- OnlineShopping/src/main/java/com/project/repository/ProductDao.java
+++ OnlineShopping/src/main/java/com/project/repository/ProductDao.java
@@ -1,7 +1,6 @@
 package com.project.repository;
 
 import java.util.List;
-
 import com.project.dto.ProductDto;
 import com.project.entity.Product;
 
@@ -9,5 +8,6 @@ public interface ProductDao {
 
 	public List<ProductDto> search(String keyword);
 	public List<ProductDto> sortProductByFactors(String by , boolean order ); 
-	public ProductDto getProductById(int productId);
+	//public ProductDto getProductById(int productId);
+	public ProductDto getProductById();
 }
--- OnlineShopping/src/main/java/com/project/service/ProductService.java
+++ OnlineShopping/src/main/java/com/project/service/ProductService.java
@@ -1,7 +1,6 @@
 package com.project.service;
 
 import java.util.List;
-
 import com.project.dto.ProductDto;
 
 
@@ -9,6 +8,7 @@ public interface ProductService {
 
 	public List<ProductDto> sortProduct(String by, boolean order);
 	public List<ProductDto> search(String keyword);
-	List<ProductDto> toListAllProducts(int productId); 
+	//ProductDto toListAllProducts(int productId); 
+	ProductDto toListAllProducts(); 
 
 }
