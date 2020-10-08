77e00f5c37ccb691cca697bf89ff162f06ef2dcf
musukha20 <musukhab2@gmail.com>
2020-10-08 15:59:24 +0530

    Added wishList Dto

---
+++ OnlineShopping/src/main/java/com/project/dto/WishListDto.java
@@ -0,0 +1,48 @@
+package com.project.dto;
+
+public class WishListDto {
+	private int wId;
+	private int pId;
+	private String pImage1;
+	private String pName;
+	private String pBrand;
+	private int pPrice;
+	public int getwId() {
+		return wId;
+	}
+	public void setwId(int wId) {
+		this.wId = wId;
+	}
+	public int getpId() {
+		return pId;
+	}
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+	public String getpImage1() {
+		return pImage1;
+	}
+	public void setpImage1(String pImage1) {
+		this.pImage1 = pImage1;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getpBrand() {
+		return pBrand;
+	}
+	public void setpBrand(String pBrand) {
+		this.pBrand = pBrand;
+	}
+	public int getpPrice() {
+		return pPrice;
+	}
+	public void setpPrice(int pPrice) {
+		this.pPrice = pPrice;
+	}
+	
+
+}
